3b33c0fa93b73c8836d43dcfa5896100981cc3c7
John Bascom <j.bascom@live.com>
2026-06-07 07:00:04 -0400

    chore: bump version to 2026.6.22

--- build-release.ps1
+++ build-release.ps1
@@ -7,7 +7,7 @@
 # whole folder and resolves the main exe via --mainExe.
 
 $ErrorActionPreference = "Stop"
-$version = "2026.6.21"
+$version = "2026.6.22"
 $project = "AllsioPush.App"
 $rid = "win-x64"
 $publishDir = "publish\$rid"
